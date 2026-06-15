912f06657b3dddeb171c8359f37041e3c7e627e0
Bya Sebastien <sebastien.bya@bstorm.be>
2026-06-15 10:40:51 +0200

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TF_NET_2026_IAC_Bend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
